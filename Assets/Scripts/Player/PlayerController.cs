d56e9d3edd956b65a38e5556421fd79c6d477edb
Xwilarg <xwilarg@protonmail.com>
2024-03-23 14:05:16 +0100

    Add player

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,56 @@
+using MicroJam10.SO;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MicroJam10.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerInfo _info;
+
+        private Vector2 _mov;
+        private float _verticalSpeed;
+        private CharacterController _controller;
+
+        private void Awake()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
+
+        private void FixedUpdate()
+        {
+            var pos = _mov;
+            Vector3 desiredMove = transform.forward * pos.y + transform.right * pos.x;
+
+            // Get a normal for the surface that is being touched to move along it
+            Physics.SphereCast(transform.position, _controller.radius, Vector3.down, out RaycastHit hitInfo,
+                               _controller.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+            desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+            Vector3 moveDir = Vector3.zero;
+            moveDir.x = desiredMove.x * _info.ForceMultiplier;
+            moveDir.z = desiredMove.z * _info.ForceMultiplier;
+
+            if (_controller.isGrounded && _verticalSpeed < 0f) // We are on the ground and not jumping
+            {
+                moveDir.y = -.1f; // Stick to the ground
+                _verticalSpeed = -_info.GravityMultiplicator;
+            }
+            else
+            {
+                // We are currently jumping, reduce our jump velocity by gravity and apply it
+                _verticalSpeed += Physics.gravity.y * _info.GravityMultiplicator;
+                moveDir.y += _verticalSpeed;
+            }
+
+            var p = transform.position;
+            _controller.Move(moveDir);
+        }
+
+        public void OnMove(InputAction.CallbackContext value)
+        {
+            _mov = value.ReadValue<Vector2>();
+        }
+    }
+}
---
+++ Assets/Scripts/SO/PlayerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace MicroJam10.SO
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/PlayerInfo", fileName = "PlayerInfo")]
+    public class PlayerInfo : ScriptableObject
+    {
+        [Range(0f, 10f)]
+        [Tooltip("Speed of the player")]
+        public float ForceMultiplier = 1f;
+
+        [Tooltip("Gravity multiplier to make the player fall")]
+        public float GravityMultiplicator;
+    }
+}
