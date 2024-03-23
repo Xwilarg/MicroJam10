2e059ef6c54b8f1e1b2c55e1a0d3113189ab42d4
Xwilarg <xwilarg@protonmail.com>
2024-03-23 21:10:35 +0100

    Add pentacle middle light

---
+++ Assets/Scripts/Craft/ExecutionSpot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace MicroJam10.Craft
+{
+    public class ExecutionSpot : MonoBehaviour
+    {
+        private Light _light;
+
+        [SerializeField]
+        private GameObject _bloodMiddle;
+
+        private void Awake()
+        {
+            _light = GetComponent<Light>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _light.enabled = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _light.enabled = false;
+            }
+        }
+    }
+}
