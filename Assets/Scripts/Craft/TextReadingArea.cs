3c1a523fd789aaee3cda0032b0c53bc62faf10a8
Xwilarg <xwilarg@protonmail.com>
2024-03-23 19:12:04 +0100

    Add camera effect

---
+++ Assets/Scripts/Craft/TextReadingArea.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace MicroJam10.Craft
+{
+    public class TextReadingArea : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _worldCamContainer, _textCamContainer;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _worldCamContainer.SetActive(false);
+                _textCamContainer.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _worldCamContainer.SetActive(true);
+                _textCamContainer.SetActive(false);
+            }
+        }
+    }
+}
